9412290a50bca258c272a6f383d73f8eb4d127be
zigelzi <miika.a.savela@gmail.com>
2020-09-13 10:00:44 +0300

    Initial draft of the site

---
+++ src/components/Card.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card f-grow">
+    <h1 class="card-title mb-s">{{ title }}</h1>
+    <p
+      class="card-subtext text-capitalized"
+      v-for="(task, index) in subtext"
+      :key="index"
+    >
+      {{ task }}
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    title: String,
+    subtext: Array,
+  },
+}
+</script>
+<style lang="scss">
+.card {
+  max-width: 250px;
+  background: var(--gray-100);
+  margin: 0 var(--mg-m) var(--mg-m) 0;
+  padding: var(--p-m);
+}
+
+.card-title {
+  font-size: var(--text-20);
+}
+
+.card-subtext {
+  color: var(--gray-400);
+  font-size: var(--text-12);
+  display: inline-block;
+  margin: 0 var(--mg-s) 0 0;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,105 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <section class="hero-container flex f-jac">
+      <div class="hero-content text-center">
+        <h1 class="mb-m">Pelipojat</h1>
+        <h2 class="text-m">Pelistudio harrastuksena</h2>
+      </div>
+    </section>
+    <section>
+      <div class="text-content">
+        <h1 class="mb-l">The boys</h1>
+        <div class="flex f-wrap">
+          <s-card
+            v-for="crewmember in crew"
+            :key="crewmember.id"
+            :title="crewmember.name"
+            :subtext="crewmember.tasks"
+          />
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="text-content">
+        <h1>The Games</h1>
+        <div class="mv-xl" v-for="(game, index) in games" :key="index">
+          <h1 class="text-l mb-s">{{ game.title }}</h1>
+          <p class="mv-xs">{{ game.description }}</p>
+          <g-link :to="game.slug">Lue lisää > </g-link>
+        </div>
+      </div>
+    </section>
   </Layout>
 </template>
 
 <script>
+import Card from '@/components/Card'
 export default {
   metaInfo: {
-    title: 'Hello, world!'
-  }
+    title: 'Index',
+  },
+  components: {
+    's-card': Card,
+  },
+  data() {
+    return {
+      crew: [
+        {
+          id: 1,
+          name: 'Miika Savela',
+          tasks: ['gamedev', 'suunnittelu', 'kahvin juominen', 'testaus'],
+        },
+        {
+          id: 2,
+          name: 'Tuomas Kajander',
+          tasks: ['ideointi', 'tutkimus', 'testaus'],
+        },
+        {
+          id: 3,
+          name: 'Joni Grönthal',
+          tasks: ['testaus', 'tutkimus', 'miikan hillitseminen'],
+        },
+        {
+          id: 4,
+          name: 'Anssi Ronkainen',
+          tasks: ['ideointi', 'suunnittelu', 'huumori', 'tutkimusmetodologia'],
+        },
+        {
+          id: 5,
+          name: 'Santtu Heleä',
+          tasks: ['kikkailu', 'testaus', 'webdev'],
+        },
+        {
+          id: 6,
+          name: 'Kalle Paakkunainen',
+          tasks: ['farmaaminen', 'testaus', 'ideointi'],
+        },
+      ],
+      games: [
+        {
+          title: 'Prototype 1',
+          description: 'Lyhyt ja simppeli kuvaus prototyypistä',
+          slug: 'proto-1',
+        },
+        {
+          title: 'Prototype 2',
+          description: 'Lyhyt ja simppeli kuvaus prototyypistä',
+          slug: 'proto-2',
+        },
+        {
+          title: 'Prototype 3',
+          description: 'Lyhyt ja simppeli kuvaus prototyypistä',
+          slug: 'proto-3',
+        },
+      ],
+    }
+  },
 }
 </script>
 
 <style>
-.home-links a {
-  margin-right: 1rem;
+.hero-container {
+  height: 90vh;
+  flex-direction: column;
 }
 </style>
